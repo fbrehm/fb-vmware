2419dd1ae664f1c8e1042403f037f34e7bddd13c
Frank Brehm <frank@brehm-online.com>
2022-04-28 16:18:55 +0200

    Adding test/test_10-base.py for testing module fb_vmeare.base

---
+++ test/test_10-base.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@author: Frank Brehm
+@contact: frank@brehm-online.com
+@copyright: © 2022 Frank Brehm, Berlin
+@license: GPL3
+@summary: test script (and module) for unit tests on module fb_vmware.base
+'''
+
+import os
+import sys
+import logging
+
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+libdir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'lib'))
+sys.path.insert(0, libdir)
+
+from general import FbVMWareTestcase, get_arg_verbose, init_root_logger
+
+LOG = logging.getLogger('test-base')
+
+
+# =============================================================================
+class TestVMWareBase(FbVMWareTestcase):
+
+    # -------------------------------------------------------------------------
+    def setUp(self):
+        pass
+
+    # -------------------------------------------------------------------------
+    def test_import(self):
+
+        if self.verbose >= 1:
+            print()
+        LOG.info("Testing import of fb_vmware.base ...")
+        import fb_vmware.base
+        from fb_vmware import BaseVsphereHandler                # noqa
+
+        LOG.debug("Version of fb_vmware.base: {!r}.".format(fb_vmware.base.__version__))
+
+
+# =============================================================================
+if __name__ == '__main__':
+
+    verbose = get_arg_verbose()
+    if verbose is None:
+        verbose = 0
+    init_root_logger(verbose)
+
+    LOG.info("Starting tests ...")
+
+    suite = unittest.TestSuite()
+
+    suite.addTest(TestVMWareBase('test_import', verbose))
+
+    runner = unittest.TextTestRunner(verbosity=verbose)
+
+    result = runner.run(suite)
+
+# =============================================================================
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4

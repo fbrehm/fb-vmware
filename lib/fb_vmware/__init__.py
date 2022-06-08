9bc8266398751ddfe4630b0897102727b5f943a1
Frank Brehm <frank@brehm-online.com>
2022-06-08 16:06:16 +0200

    Adding script get-vsphere-vm-list and its appropriate application class

--- lib/fb_vmware/__init__.py
+++ lib/fb_vmware/__init__.py
@@ -58,7 +58,7 @@ from .config import DEFAULT_CONFIG_DIR, DEFAULT_VSPHERE_PORT                    
 from .config import DEFAULT_VSPHERE_USER, DEFAULT_VSPHERE_DC                    # noqa
 from .config import DEFAULT_VSPHERE_CLUSTER                                     # noqa
 
-__version__ = '0.4.5'
+__version__ = '0.4.6'
 
 LOG = logging.getLogger(__name__)
 

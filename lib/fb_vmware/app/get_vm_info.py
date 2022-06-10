d2978a348d84127c727a1e5ed2bfe5f97a5995f2
Frank Brehm <frank@brehm-online.com>
2022-06-10 11:37:47 +0200

    Splitting method GetVmApplication.show_vm() in sub methods

--- lib/fb_vmware/app/get_vm_info.py
+++ lib/fb_vmware/app/get_vm_info.py
@@ -28,7 +28,7 @@ from ..controller import VsphereDiskController
 
 from ..ether import VsphereEthernetcard
 
-__version__ = '1.6.1'
+__version__ = '1.6.2'
 LOG = logging.getLogger(__name__)
 
 _ = XLATOR.gettext
@@ -127,23 +127,8 @@ class GetVmApplication(BaseVmwareApplication):
         print('\n{}: '.format(vm_name), end='')
         if self.verbose:
             print()
-        vm = None
-        for vsphere_name in self.vsphere:
-            vsphere = self.vsphere[vsphere_name]
-            vm = vsphere.get_vm(vm_name, vsphere_name=vsphere_name, no_error=True, as_obj=True)
-            vm.full_custom_data = []
-            if vm.custom_data:
-                for cdata in vm.custom_data:
-                    for custom_key in cdata.keys():
-                        custom_value = cdata[custom_key]
-                        custom_name = vsphere.custom_field_name(custom_key)
-                        if custom_name is None:
-                            custom_name = custom_key
-                        vm.full_custom_data.append({custom_name: custom_value})
 
-            if vm:
-                break
-
+        vm = self._get_vm_data(vm_name)
         if not vm:
             print(self.colored(_("NOT FOUND"), 'RED'))
             return False
@@ -179,6 +164,16 @@ class GetVmApplication(BaseVmwareApplication):
 
         print("    OS:       {id}    {os}".format(id=os_id, os=os_name))
 
+        self._print_ctrlrs(vm)
+        self._print_disks(vm)
+        self._print_interfaces(vm)
+        self._print_custom_data(vm)
+
+        return True
+
+    # -------------------------------------------------------------------------
+    def _print_ctrlrs(self, vm):
+
         first = True
         for ctrlr in sorted(
                 filter(lambda x: x.scsi_ctrl_nr is not None, vm.controllers),
@@ -202,64 +197,103 @@ class GetVmApplication(BaseVmwareApplication):
                 la=label, nr=ctrlr.bus_nr, di=no_disk, ty=ctype)
             print(msg)
 
-        if vm.disks:
-            total_gb = 0.0
-            first = True
-            for disk in vm.disks:
-                total_gb += disk.size_gb
-                label = ' ' * 15
-                if first:
-                    label = (ngettext('Disk', 'Disks', len(vm.disks)) + ':').ljust(15)
-                first = False
-                ctrlr_nr = -1
-                for ctrlr in vm.controllers:
-                    if disk.key in ctrlr.devices:
-                        ctrlr_nr = ctrlr.bus_nr
-                        break
-                msg = "    {la}  {n:<15} - {s:7.1f} GiB - Controller {c:>2} - File {f}".format(
-                    la=label, n=disk.label, s=disk.size_gb, c=ctrlr_nr, f=disk.file_name)
-                print(msg)
-            if len(vm.disks) > 1:
-                msg = (' ' * 21) + '{n:<15} - {s:7.1f} GiB'.format(n=_('Total'), s=total_gb)
-                print(msg)
-        else:
+    # -------------------------------------------------------------------------
+    def _print_disks(self, vm):
+
+        if not vm.disks:
             print("    Disks:       {}".format(_('None')))
+            return
 
-        if vm.interfaces:
-            first = True
-            for dev in vm.interfaces:
-                label = ' ' * 15
-                if first:
-                    label = 'Ethernet:'.ljust(15)
-                first = False
-                etype = _('Unknown')
-                if dev.ether_type in VsphereEthernetcard.ether_types.keys():
-                    etype = VsphereEthernetcard.ether_types[dev.ether_type]
-                msg = "    {la}  {n:<15} - Network {nw:<20} - Connection: {c:<4} - {t}".format(
-                    la=label, n=dev.label, nw=dev.backing_device, c=dev.connect_status, t=etype)
-                print(msg)
-        else:
+        total_gb = 0.0
+        first = True
+        for disk in vm.disks:
+            total_gb += disk.size_gb
+            label = ' ' * 15
+            if first:
+                label = (ngettext('Disk', 'Disks', len(vm.disks)) + ':').ljust(15)
+            first = False
+            ctrlr_nr = -1
+            for ctrlr in vm.controllers:
+                if disk.key in ctrlr.devices:
+                    ctrlr_nr = ctrlr.bus_nr
+                    break
+            msg = "    {la}  {n:<15} - {s:7.1f} GiB - Controller {c:>2} - File {f}".format(
+                la=label, n=disk.label, s=disk.size_gb, c=ctrlr_nr, f=disk.file_name)
+            print(msg)
+        if len(vm.disks) > 1:
+            msg = (' ' * 21) + '{n:<15} - {s:7.1f} GiB'.format(n=_('Total'), s=total_gb)
+            print(msg)
+
+    # -------------------------------------------------------------------------
+    def _print_interfaces(self, vm):
+
+        if not vm.interfaces:
             print("    Ethernet:    {}".format(_('None')))
+            return
 
-        if vm.custom_data:
-            no_vals = len(vm.custom_data)
-            label = ngettext('Custom Value', 'Custom Values', no_vals)
-            print("    {}:".format(label))
-            max_key_len = 1
-            for custom_data in vm.full_custom_data:
-                for custom_name in custom_data.keys():
-                    if len(custom_name) > max_key_len:
-                        max_key_len = len(custom_name)
-            max_key_len += 1
-            for custom_data in vm.full_custom_data:
-                for custom_name in custom_data.keys():
-                    custom_value = custom_data[custom_name]
-                    name = custom_name + ':'
-                    line = '        - {n:<{len}} {val}'.format(
-                        n=name, len=max_key_len, val=custom_value)
-                    print(line.rstrip())
+        first = True
+        for dev in vm.interfaces:
+            label = ' ' * 15
+            if first:
+                label = 'Ethernet:'.ljust(15)
+            first = False
+            etype = _('Unknown')
+            if dev.ether_type in VsphereEthernetcard.ether_types.keys():
+                etype = VsphereEthernetcard.ether_types[dev.ether_type]
+            msg = "    {la}  {n:<15} - Network {nw:<20} - Connection: {c:<4} - {t}".format(
+                la=label, n=dev.label, nw=dev.backing_device, c=dev.connect_status, t=etype)
+            print(msg)
 
-        return True
+    # -------------------------------------------------------------------------
+    def _print_custom_data(self, vm):
+
+        if not vm.custom_data:
+            return
+
+        no_vals = len(vm.custom_data)
+
+        label = ngettext('Custom Value', 'Custom Values', no_vals)
+        print("    {}:".format(label))
+
+        max_key_len = 1
+        for custom_data in vm.full_custom_data:
+            for custom_name in custom_data.keys():
+                if len(custom_name) > max_key_len:
+                    max_key_len = len(custom_name)
+        max_key_len += 1
+        for custom_data in vm.full_custom_data:
+            for custom_name in custom_data.keys():
+                custom_value = custom_data[custom_name]
+                name = custom_name + ':'
+                line = '        - {n:<{len}} {val}'.format(
+                    n=name, len=max_key_len, val=custom_value)
+                print(line.rstrip())
+
+    # -------------------------------------------------------------------------
+    def _get_vm_data(self, vm_name):
+
+        if self.verbose > 1:
+            LOG.debug(_("Pulling full data of VM {!r} ...").format(vm_name))
+
+        vm = None
+
+        for vsphere_name in self.vsphere:
+            vsphere = self.vsphere[vsphere_name]
+            vm = vsphere.get_vm(vm_name, vsphere_name=vsphere_name, no_error=True, as_obj=True)
+            vm.full_custom_data = []
+            if vm.custom_data:
+                for cdata in vm.custom_data:
+                    for custom_key in cdata.keys():
+                        custom_value = cdata[custom_key]
+                        custom_name = vsphere.custom_field_name(custom_key)
+                        if custom_name is None:
+                            custom_name = custom_key
+                        vm.full_custom_data.append({custom_name: custom_value})
+
+            if vm:
+                break
+
+        return vm
 
 
 # =============================================================================
